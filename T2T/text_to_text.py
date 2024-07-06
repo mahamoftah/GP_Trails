e0eb2518cefdbaf4bf88ba5cae8d0206bcbfdbfd
mahamoftah <mahamoftah19@gmail.com>
2024-07-06 03:32:06 +0300

    T2T

--- T2T/text_to_text.py
+++ T2T/text_to_text.py
@@ -1,22 +1,51 @@
 import requests
 from langchain_community.llms import Ollama
+from groq import Groq
+import json
 
 
-def text_to_text_ollam_api(text, model):
+def text_to_text_ollam_api(question, model):
+    quest = [{
+        "role": "user",
+        "content": question,
+    }]
+
     response = requests.post(
         "http://localhost:11434/api/chat",
         json={
             "model": model,
-            "messages": text,
+            "messages": quest,
             "stream": False
         }
     )
+
     resp = response.json()
     return resp["message"]['content']
 
 
-def text_to_text_ollam_langchain(text, model):
+def text_to_text_ollam_langchain(question, model):
+    quest = [{
+        "role": "user",
+        "content": question,
+    }]
 
     llm = Ollama(model=model)
-    return llm.invoke(text)
+    return llm.invoke(quest)
 
+
+def text_to_text_groq(question, model):
+
+    groq = Groq(api_key="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")
+    quest = [{
+        "role": "user",
+        "content": question,
+    }]
+
+    response = groq.chat.completions.create(
+        messages=quest,
+        model=model,
+        temperature=0,
+        stream=False,
+    )
+
+    return response.choices[0].message.content
